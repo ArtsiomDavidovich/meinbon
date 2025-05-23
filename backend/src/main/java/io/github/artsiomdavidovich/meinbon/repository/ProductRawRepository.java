044c337f803d89b85a193cf53dcc562a30beccd2
Artem Davidovich <chumazek@gmail.com>
2025-05-23 12:12:38 +0200

    Repositories

---
+++ backend/src/main/java/io/github/artsiomdavidovich/meinbon/repository/ProductRawRepository.java
@@ -0,0 +1,7 @@
+package io.github.artsiomdavidovich.meinbon.repository;
+
+import io.github.artsiomdavidovich.meinbon.domain.entity.ProductRaw;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRawRepository extends JpaRepository<ProductRaw, Long> {
+}
---
+++ backend/src/main/java/io/github/artsiomdavidovich/meinbon/repository/ReceiptRepository.java
@@ -0,0 +1,10 @@
+package io.github.artsiomdavidovich.meinbon.repository;
+
+import io.github.artsiomdavidovich.meinbon.domain.entity.Receipt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
+
+
+
+}
